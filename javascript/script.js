210430becfa1f427ec033cf2c49c016c392fb88b
Saayana Desain <75817208+saayanadesain@users.noreply.github.com>
2021-04-07 14:03:22 +0700

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,4 @@
-// $(document).ready(function() {
-//   $('#autoWidth').lightSlider({
-//       autoWidth:true,
-//       loop:true,
-//       onSliderLoad: function() {
-//           $('#autoWidth').removeClass('cS-hidden');
-//       }
-//   });
-// });
-
+// Slide
 $(document).ready(function() {
     var autoplaySlider = $('#autoplay').lightSlider({
         autoWidth:true,
@@ -21,14 +12,48 @@ $(document).ready(function() {
     $('#total').text(autoplaySlider.getTotalSlideCount());
 });
 
+// loadMore
 $(document).ready(function(){
-  $(".content").slice(0, 12).show();
+  $(".content").slice(0, 20).show();
   $("#loadMore").on("click", function(e){
     e.preventDefault();
-    $(".content:hidden").slice(0, 12).slideDown();
+    $(".content:hidden").slice(0, 20).slideDown();
     if($(".content:hidden").length == 0) {
       $("#loadMore").text("No Content").addClass("noContent");
     }
   });
+})
 
+// Sorting
+$(document).ready(function(){
+  var list, i, switching, b, shouldSwitch;
+  list = document.getElementById("id01");
+  switching = true;
+  /* Make a loop that will continue until
+  no switching has been done: */
+  while (switching) {
+    // start by saying: no switching is done:
+    switching = false;
+    b = list.getElementsByTagName("LI");
+    // Loop through all list-items:
+    for (i = 0; i < (b.length - 1); i++) {
+      // start by saying there should be no switching:
+      shouldSwitch = false;
+      /* check if the next item should
+      switch place with the current item: */
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        /* if next item is alphabetically
+        lower than current item, mark as a switch
+        and break the loop: */
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /* If a switch has been marked, make the switch
+      and mark the switch as done: */
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
 })
